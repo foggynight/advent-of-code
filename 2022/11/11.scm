ea978bb03de7581ae364ac0cfbbab6b8aede5039
Robert Coffey <rjc@heretyk.xyz>
2022-12-12 08:23:54 -0330

    add 2022/11 part 2

--- 2022/11/11.scm
+++ 2022/11/11.scm
@@ -1,4 +1,4 @@
-;; 11.scm - Robert Coffey - 2022-12-11
+;; 11.scm - Robert Coffey - 2022-12-11 / 2022-12-12
 
 (import (chicken format)
         (chicken io)
@@ -23,14 +23,13 @@
 (define (monkey-add-item! monkey item)
   (monkey-items-set! monkey (cons item (monkey-items monkey))))
 
-(define (monkey-inspect-items! monkey monkeys)
+(define (monkey-inspect-items! monkey monkeys prod)
   (define (inspect! item)
     (let* ((worry ((monkey-op monkey) item))
-           (targ ((if ((monkey-test monkey) worry)
-                      monkey-true
-                      monkey-false)
-                  monkey)))
-      (monkey-add-item! (vector-ref monkeys targ) worry)))
+           (true (zero? (modulo worry (monkey-test monkey))))
+           (targ ((if true monkey-true monkey-false) monkey)))
+      (monkey-add-item! (vector-ref monkeys targ)
+                        (modulo worry prod))))
   (let loop ((items (reverse (monkey-items monkey))))
     (unless (null? items)
       (inspect! (car items))
@@ -44,8 +43,8 @@
                     old
                     (string->number arg)))
            (sp ((if (char=? (string-ref op 0) #\+) + *) old val)))
-      (quotient sp 3)   ; part 1
-      #;sp                ; part 2
+      #;(quotient sp 3)   ; part 1
+      sp                ; part 2
       )))
 
 (define (parse-monkey lines)
@@ -55,9 +54,7 @@
      (let ((items (reverse (map string->number
                                 (cddr (string-split l-items " ,")))))
            (op (apply parse-op (drop (string-split l-op) 4)))
-           (test (lambda (new)
-                   (let ((d (last-number l-test)))
-                     (zero? (modulo new d)))))
+           (test (last-number l-test))
            (true (last-number l-true))
            (false (last-number l-false)))
        (make-monkey 0 items op test true false)))))
@@ -65,10 +62,10 @@
 (define (parse-monkeys lines)
   (list->vector (map parse-monkey (chop lines 7))))
 
-(define (update-monkeys! monkeys)
+(define (update-monkeys! monkeys prod)
   (do ((i 0 (+ i 1)))
       ((= i (vector-length monkeys)))
-    (monkey-inspect-items! (vector-ref monkeys i) monkeys)))
+    (monkey-inspect-items! (vector-ref monkeys i) monkeys prod)))
 
 (define (monkey-business monkeys)
   (define mks (sort monkeys (lambda (m1 m2)
@@ -83,21 +80,23 @@
   (define lines (with-input-from-file path read-lines))
   (define monkeys (parse-monkeys lines))
 
+  (define prod (vector-fold (lambda (i a m) (* a (monkey-test m))) 1 monkeys))
+
   ;; Part 1
-  (print-monkeys monkeys)
-  (do ((i 0 (+ i 1))) ((= i 20))
-    (printf "Round ~A...\n" (+ i 1))
-    (update-monkeys! monkeys)
-    (print-monkeys monkeys))
-  (printf "Part 1: ~A\n" (monkey-business monkeys))
+  ;(print-monkeys monkeys)
+  ;(do ((i 0 (+ i 1))) ((= i 20))
+  ;  (printf "Round ~A...\n" (+ i 1))
+  ;  (update-monkeys! monkeys prod)
+  ;  (print-monkeys monkeys))
+  ;(printf "Part 1: ~A\n" (monkey-business monkeys))
 
   ;; Part 2
-  ;(print-monkeys monkeys)
-  ;(do ((i 0 (+ i 1))) ((= i 1000))
-  ;  (printf "Round ~A...\n" (+ i 1))
-  ;  (update-monkeys! monkeys)
-  ;  (print-monkeys monkeys))
-  ;(printf "Part 2: ~A\n" (monkey-business monkeys))
+  (print-monkeys monkeys)
+  (do ((i 0 (+ i 1))) ((= i 10000))
+    (printf "Round ~A...\n" (+ i 1))
+    (update-monkeys! monkeys prod)
+    (print-monkeys monkeys))
+  (printf "Part 2: ~A\n" (monkey-business monkeys))
   )
 
 (main "11.dat")
